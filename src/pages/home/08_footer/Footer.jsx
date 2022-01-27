407c9d7d1213cb1ab291a6778a7da5b7630f6fba
wave64 <hippiengypsy02@gmail.com>
2022-01-27 16:41:05 +0900

    [temp] Footer section responsive finished.

--- src/pages/home/08_footer/Footer.jsx
+++ src/pages/home/08_footer/Footer.jsx
@@ -1,6 +1,5 @@
 import Styles from "./Footer.module.scss";
 
-import { Container, Row, Col } from 'react-bootstrap'
 
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -10,25 +9,22 @@ import FooterLogo from "../../../assets/botlogo.gif";
 
 const Footer = () => {
     return (
-        <div className={Styles.footer}>
-            <Container>
-                <Row className={Styles.footer}>
-                    <Col>
-                        <img src={FooterLogo} alt="" className="logo"/>
-                    </Col>
-                    <Col md={6}>
-                        <p>Copyright ⓒ 2014 BIGBAND. All rights reserved. </p>
-                    </Col>
-                    <Col>
-                        <div className={Styles.footer__icons}>
-                            <YouTubeIcon fontSize='large'/>
-                            <FacebookIcon fontSize='large'/>
-                            <TwitterIcon fontSize='large'/>
-                        </div>
-                    </Col>
-                </Row>
-
-            </Container>
+        <div className={Styles.footer__container}>
+            <div className={Styles.footer__flexbox}>
+                <div className={Styles.footer__item}>
+                    <img src={FooterLogo} alt="" />
+                </div>
+                <div className={Styles.footer__item}>
+                    <p>Copyright ⓒ 2014 BIGBAND. All rights reserved. </p>
+                </div>
+                <div className={Styles.footer__item__right}>
+                    <div className={Styles.footer__icons}>
+                        <YouTubeIcon fontSize='large'/>
+                        <FacebookIcon fontSize='large'/>
+                        <TwitterIcon fontSize='large'/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
